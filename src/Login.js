949335f5e8667eaab5e9f7479817d86eb2e0f774
Alexandre <alexandre1arnaud@gmail.com>
2019-04-09 10:38:04 +0200

    Auto stash before merge of "Alex" and "origin/master"

---
+++ src/Login.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        <header>
+          Test
+          <nav>
+            
+          </nav>
+        </header>
+
+        <body>
+          <button>New button</button>
+        </body>
+
+        <footer>
+          Test2
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Login;
